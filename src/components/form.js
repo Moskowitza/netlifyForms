92050bcd4d8a1703eccc1f57fd347b42696c908f
moskowitza <moskowitzaaron@mac.com>
2019-08-11 09:03:17 -0400

    work on form

--- src/components/form.js
+++ src/components/form.js
@@ -1,16 +1,30 @@
-import React from "react"
+import React, { useState } from "react"
 
 const Form = () => {
+  const [userName, setUserName] = useState("")
+  const [email, setEmail] = useState("")
   return (
     <form name="contact" netlify>
       <p>
-        <label>
-          Name           <input type="text" name="name" />
+        <label htmlFor="userName">
+          User Name
+          <input
+            type="text"
+            name="userName"
+            value={userName}
+            onChange={e => setUserName(e.target.value)}
+          />
         </label>
       </p>
       <p>
-        <label>
-          Email           <input type="email" name="email" />
+        <label htmlFor="email">
+          Email
+          <input
+            type="email"
+            name="email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
         </label>
       </p>
       <p>
